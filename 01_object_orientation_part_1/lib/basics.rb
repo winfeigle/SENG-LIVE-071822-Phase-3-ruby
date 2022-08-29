3198c02d7477a879145a8672e847f4d49c69868c
ixnp <enprocopios@gmail.com>
2022-08-29 14:01:38 -0700

    01 solution

---
+++ 01_object_orientation_part_1/lib/basics.rb
@@ -0,0 +1,39 @@
+require 'pry'
+#Ruby Datatypes
+# Numbers
+    #int 1
+    #float 1.0
+# Strings
+    # 'cat'
+    # "cat"
+# Boolean
+    #true
+    #false
+# Symbols 
+     # :cat
+# nil
+
+# Arrays
+    #[]
+# Hashes 
+    #{}
+
+name = 'rose'
+
+puts("my cat #{name} is cute!")
+
+my_hash = {cat:'rose'}
+puts(my_hash[:cat])
+
+# function foo(){
+#     return 'hello wolrd'
+# }
+
+def hi
+    "hello world"
+    "hi"
+end 
+
+cat = nil
+puts(cat)
+binding.pry
--- 01_object_orientation_part_1/lib/dog.rb
+++ 01_object_orientation_part_1/lib/dog.rb
@@ -1,13 +1,58 @@
+require 'pry'
 class Dog
-  # ✅ we should be able to create dogs with a name, age, breed, and image_url
+  # attr_reader :name, :age, :image_url, :breed
+  # attr_writer :name, :age, :image_url
+  attr_reader :breed
+  attr_accessor :name, :age, :image_url
 
+  # ✅ we should be able to create dogs with a name, age, breed, and image_url
+  def initialize(name, age, image_url, breed)
+    @name = name
+    @age = age
+    @image_url = image_url
+    @breed = breed
+  end 
+
+  # Mass Assignment - optional reviewed at the end of the video,  
+  # def initialize(attributes)
+  #   attributes.each do |attribute, value|
+  #     self.send("#{attribute}=",value)
+  #   end 
+  # end 
   
 
   # ✅ we want to be able to access a dog's name, age, breed, image_url, last_fed_at and last_walked at times
+  # def name
+  #   @name
+  # end
 
+  # def age
+  #   @age
+  # end 
+
+  # def age=(value)
+  #   @age = value
+  # end
+
+  
 
 
   # ✅ we want to be able to print details about a dog (this time including the last walked at and last fed at times)
-
+  def print
+    puts "*************************"
+    puts "Name: #{@name}"
+    puts "Age: #{@age}"
+    puts "Breed: #{@breed}"
+    puts "Image: #{@image_url}"
+    puts "*************************"
+  end 
 
 end
+
+#Instances
+#Dog.new()
+d1 = Dog.new('Ash', 2, 'ash.jpg', 'husky')
+d2 = Dog.new('Lennord', 1, 'lennord.jpg', 'pomeranian')
+d3 = Dog.new('Winter', 9, 'winter.png', 'mix')
+
+binding.pry
