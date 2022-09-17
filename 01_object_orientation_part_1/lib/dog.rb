575306e4e70e2c072b81022927ae186363c04169
Win Feigle <win.feigle@gmail.com>
2022-09-16 20:01:44 -0600

    Add attributes to class

--- 01_object_orientation_part_1/lib/dog.rb
+++ 01_object_orientation_part_1/lib/dog.rb
@@ -1,11 +1,29 @@
 class Dog
-  # ✅ we should be able to create dogs with a name, age, breed, and image_url
+  attr_reader :breed
+  attr_accessor :name, :age, :image_url
 
-  
+  # ✅ we should be able to create dogs with a name, age, breed, and image_url
+  def initialize(name, age, image_url, breed)
+    @name = name
+    @age = age
+    @image_url = image_url
+    @breed = breed
+  end
 
   # ✅ we want to be able to access a dog's name, age, breed, image_url, last_fed_at and last_walked at times
+  ## Getter methods (Not necessary when using macros above)
+  # def name
+  #   @name
+  # end
 
+  # def age
+  #   @age
+  # end
 
+  ## Setter methods
+  # def age=(value)
+  #   @age = value
+  # end
 
   # ✅ we want to be able to print details about a dog (this time including the last walked at and last fed at times)
 
